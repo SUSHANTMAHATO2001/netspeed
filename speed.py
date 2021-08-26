4845908a3092d7dbd967da479541742fd8f214d5
SUSHANT <70790978+SUSHANTMAHATO2001@users.noreply.github.com>
2021-08-26 23:34:15 +0545

    Create speed.py

---
+++ speed.py
@@ -0,0 +1,33 @@
+import sys,os                                        import time                                          try:
+      import pyspeedtest                             except ImportError:
+       os.system('python2 -m pip install pyspeedtest')
+
+                                                     os.system('clear')                                   
+import pyspeedtest
+
+class color:
+        r = '\x1b[;33m'                                      g = '\x1b[;32m'
+                                                     
+def susan(s):
+  for a in s + '\n':
+      sys.stdout.write(a)
+      sys.stdout.flush()
+      time.sleep(1./12)
+
+susan(color.r + "Program By Sushant Mahato")
+
+susan("Devlop By Nepal")
+
+time.sleep(1)
+os.system('clear')
+
+
+
+
+
+a = raw_input('inter url:- ')
+
+st = pyspeedtest.SpeedTest(a)
+a = st.ping()
+print(color.g)
+print(a)
